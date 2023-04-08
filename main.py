85bcc10a6c646e1312cd929b708cb0878c3f0c1d
Nick Roussis <nikosnick160@gmail.com>
2023-04-08 19:38:17 +0300

    cleaned up and improved main.py

--- main.py
+++ main.py
@@ -1,111 +1,109 @@
 # Name: Turtlesketch
-# Description: Draw simple sketches with console input or macro file, using the turtle library
+# Description: Draw simple sketches with console input or a macro file, using the python turtle library
 # Author: Nick Roussis (Neek8044)
 # Github: https://github.com/neek8044/
 
 # This is a very old project I found on my hard drive. I will try fixing it a little on later commits.
 
 import turtle as turtle
-from threading import Thread
+import threading
+
+
+__banner__ = """
+Coded by Nick Roussis (Neek8044)
+License: Apache 2.0
+Github: https://github.com/neek8044/turtlesketch
+
+COMMANDS:
+\tmove [pixels] - Move forward for specified distance, backward with a negative
+\tturn [degrees] - Turn right for specified degrees, left with a negative
+\ttoggle - Toggle to show pointer (pen position/rotation); useful with complex sketches
+\tswitch - Switch between normal and blank output (pen down/pen up)
+\tcolor [color] - Sets brush color to specified (hex supported); Black can be used as an eraser
+\tmacro [path to file] - Runs a macro file. Check the Github for more information about macros (look above)
+"""
+
 
 t = turtle.Turtle()
-
-########################################
-
-# future me here. i have no idea why i used separate functions to parse input
-class cli:
-	def run(command):
-		out = None
-		command = command.split(" ")
-
-		if command[0] == "turn":
-			out = "$" + command[1]
-
-		elif command[0] == "go":
-			out = "!" + command[1]
-
-		elif command[0] == "toggle":
-			out = "toggle"
-
-		elif command[0] == "switch":
-			out = "switch"
-
-		elif command[0] == "set":
-			out = "&" + command[1]
-
-		else:
-			return "error"
-		return out
-
-def check_console():
-	tur_shown = 0
-	penup = 0
-
-	print(
-		"""
-		MADE BY NEEK8044
-                                                                   
-		COMMANDS:
-		\tgo [pixels] - Forwards for specified distance, backwards with a negative
-		\tturn [degrees] - Right for specified degrees, left with a negative
-		\ttoggle - Toggle to show drawing pointer; useful with complex sketches
-		\tswitch - Switch between normal and blank output (pen down/pen up)
-		\tset [color] - Sets brush to specified color; Black can be used as an eraser
-
-		"""
-	)
-
-	####################
-
-	while True:
-		out = cli.run(input("Command: "))
-
-		if out == "error":
-			print("Please check spelling and try again.")
-
-		elif out == "toggle":
-			if tur_shown == 1:
-				t.hideturtle()
-				tur_shown = 0
-			else:
-				t.showturtle()
-				tur_shown = 1
-
-		elif out == "switch":
-			if penup == 1:
-				t.pendown()
-				penup = 0
-			else:
-				t.penup()
-				penup = 1
-		
-		# future me here. i have no idea why i used symbols here
-		elif out[0] == "&":
-			out = out.replace("&", "")
-			t.color(out)
-
-		elif out[0] == "!":
-			out = out.replace("!", "")
-			t.forward(int(out))
-
-		elif out[0] == "$":
-			out = out.replace("$", "")
-			t.right(int(out))
-
-########################################
-
 t.hideturtle()
-t.tracer(1,0)
 t.pensize(5)
-
 t.color("white")
-t.bgcolor("black")
-
 t.pendown()
 
-t = Thread(target=check_console)
-t.start()
+print(__banner__)
 
-t.left(90)
 
-t.mainloop()
+def execute(command):
+    global turtle_shown
+    global penup
+
+    # Toggle pointer (turtle) on/off
+    if command[0] == "toggle":
+        if turtle_shown == 1:
+            t.hideturtle()
+            turtle_shown = 0
+        else:
+            t.showturtle()
+            turtle_shown = 1
+
+    # Enable/disable pen output
+    elif command[0] == "switch":
+        if penup == 1:
+            t.pendown()
+            penup = 0
+        else:
+            t.penup()
+            penup = 1
+
+    # Change color based on given value
+    elif command[0] == "color":
+        try:
+            t.color(command[1])
+        except turtle.TurtleGraphicsError:
+            print("ERROR: Invalid input value.")
+
+    # Move forward (or backward with a negative value)
+    elif command[0] == "move":
+        try:
+            t.forward(int(command[1]))
+        except ValueError:
+            print("ERROR: Invalid input value.")
+
+    # Turn clockwise (or counter-clockwise with a negative value)
+    elif command[0] == "turn":
+        try:
+            t.right(int(command[1]))
+        except ValueError:
+            print("ERROR: Invalid input value.")
+
+    # If no valid commands were met, print the following.
+    else:
+        print("--> Please check spelling and try again.")
+
+
+def check_console():
+    while True:
+        command = input("Command: ").split(" ")
+        
+        if command[0] == "macro":
+            try:
+                print("Executing:", command[1])
+                with open(command[1]) as macro:
+                    for line in macro:
+                        execute(line)
+            except Exception as ex:
+                print("ERROR:", ex, "\n--> Make sure you set the correct path.")
+        else:
+            execute(command)
+
+
+turtle_shown = 0
+penup = 0
+
+thread = threading.Thread(target=check_console)
+thread.start()
+
+turtle.tracer(1,0)
+turtle.bgcolor("black")
+turtle.left(90)
+turtle.mainloop()
